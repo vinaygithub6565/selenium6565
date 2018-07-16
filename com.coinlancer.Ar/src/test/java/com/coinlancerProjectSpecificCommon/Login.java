4dd4ae828cf4b710a7daf109c65b5cc444ce4818
Vinay Kumar <Vinay Kumar@DESKTOP-QO13JTD>
2018-07-16 19:39:25 +0530

    newcommit

--- com.coinlancer.Ar/src/test/java/com/coinlancerProjectSpecificCommon/Login.java
+++ com.coinlancer.Ar/src/test/java/com/coinlancerProjectSpecificCommon/Login.java
@@ -1,12 +1,10 @@
 package com.coinlancerProjectSpecificCommon;
+
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import com.commonLibraries.Base;
 import com.commonLibraries.ExtentTestManager;
@@ -14,23 +12,18 @@ import com.commonLibraries.ReadProperties;
 import com.relevantcodes.extentreports.LogStatus;
 import static com.commonLibraries.Locators.*;
 import static com.commonLibraries.Common.*;
-
+import static com.coinlancerProjectSpecificCommon.CommonProject.*;
+@Listeners(com.commonLibraries.Listener.class)
 
 public class Login extends Base
 {
-	@Test
-	 public static void Login()throws IOException {
-		String username = ReadProperties.getPropValue("Username");
-			String password = ReadProperties.getPropValue("Password");
-			String url = ReadProperties.getPropValue("url");
-			driver.get("http://13.127.90.210");
-			
+	
 			
 			
 
 		}
 
-	}
+	
 
 	
 	
